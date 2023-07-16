d5ba9e901b67646d0eb88ab5ac918c8951c80813
Elidiana Andrade <97471199+elidianaandrade@users.noreply.github.com>
2023-07-16 15:44:26 -0300

    feat: add sections main and skills

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement
+
+function changeTheme(){
+  const currentTheme = rootHtml.getAttribute("data-theme");
+
+  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+
+  toggleTheme.classList.toggle("bi-sun")
+  toggleTheme.classList.toggle("bi-moon-stars")
+}
+
+
+toggleTheme.addEventListener("click", changeTheme);
