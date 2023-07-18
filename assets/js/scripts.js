8449c5536a6d227839a5bb52cc252f178c5a3470
Elidiana Andrade <97471199+elidianaandrade@users.noreply.github.com>
2023-07-18 11:12:03 -0300

    feat: add active menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,10 @@
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement
+const currentTheme = rootHtml.getAttribute("data-theme");
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
 
 function changeTheme(){
-  const currentTheme = rootHtml.getAttribute("data-theme");
-
   currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
 
   toggleTheme.classList.toggle("bi-sun")
@@ -12,13 +13,18 @@ function changeTheme(){
 
 toggleTheme.addEventListener("click", changeTheme);
 
-const accordionHeaders = document.querySelectorAll(".accordion__header");
-
 accordionHeaders.forEach(header => {
   header.addEventListener("click", () => {
     const accordionItem = header.parentElement;
     const accordionActive = accordionItem.classList.contains("active");
 
     accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+  })
+})
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
   })
 })
