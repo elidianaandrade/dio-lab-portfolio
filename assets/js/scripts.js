190dcf7081e2a00f20b5e59f8a53cf0cda19c4b9
Elidiana Andrade <97471199+elidianaandrade@users.noreply.github.com>
2023-07-18 11:45:04 -0300

    fix: dark light theme

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,11 @@
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement
-const currentTheme = rootHtml.getAttribute("data-theme");
 const accordionHeaders = document.querySelectorAll(".accordion__header");
 const menuLinks = document.querySelectorAll(".menu__link");
 
 function changeTheme(){
+  const currentTheme = rootHtml.getAttribute("data-theme");
+
   currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
 
   toggleTheme.classList.toggle("bi-sun")
